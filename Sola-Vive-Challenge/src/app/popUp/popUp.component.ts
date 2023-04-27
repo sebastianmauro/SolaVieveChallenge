62bf1237d258a951ad73e99b2a9726b9aba59ead
sebastianmauro <andresmauro.am@gmail.com>
2023-04-26 23:43:03 -0300

    add phone image

--- Sola-Vive-Challenge/src/app/popUp/popUp.component.ts
+++ Sola-Vive-Challenge/src/app/popUp/popUp.component.ts
@@ -1,6 +1,6 @@
 import {Component, Inject, Injectable} from  '@angular/core';
-
 import {MatDialogRef, MAT_DIALOG_DATA, MatDialog} from  '@angular/material/dialog';
+
 
 @Component({
 templateUrl:  'popUp.component.html'
