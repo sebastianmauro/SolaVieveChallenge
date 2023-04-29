206e61a466da4f24f693df58633552fb1abac1b8
sebastianmauro <andresmauro.am@gmail.com>
2023-04-29 19:28:22 -0300

    component tested

--- Sola-Vive-Challenge/src/app/components/popUp/popUp.component.ts
+++ Sola-Vive-Challenge/src/app/components/popUp/popUp.component.ts
@@ -4,7 +4,6 @@ import { NOT_OK_IMG_SOURCE, OK_IMG_SOURCE } from 'src/app/statics/imgSources';
 import { NOT_OK_FIRST_SENTENCE, NOT_OK_SECOND_SENTENCE, OK_FIRST_SENTENCE, OK_SECOND_SENTECE } from 'src/app/statics/sentences';
 import { ResponseCode } from 'src/enums/responseCodes';
 
-
 @Component({
 selector: 'popUp',
 templateUrl:  'popUp.component.html'
@@ -14,7 +13,7 @@ export  class  popUpComponent {
     public secondSentence: string;
     public imgSource: string = "";
     
-    constructor(private  dialogRef:  MatDialogRef<popUpComponent>, @Inject(MAT_DIALOG_DATA) public  data:  any) {
+    constructor(public  dialogRef:  MatDialogRef<popUpComponent>, @Inject(MAT_DIALOG_DATA) public  data:  any) {
         if(data.status === ResponseCode.Success){
             this.imgSource = OK_IMG_SOURCE;
             this.firstSentence = OK_FIRST_SENTENCE;
